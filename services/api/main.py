9fc1196717c24cd370f2f9ae565623c47125aed5
𝑬𝑵𝑮. 𝑬𝑳𝒀𝑨𝑺 𝑨𝑳 𝑺𝑯𝑨𝑬𝑩𝑰 <em775377979@gmail.com>
2026-08-25 03:50:53 +0300

    feat: mount learning activities and approved media routers

--- services/api/main.py
+++ services/api/main.py
@@ -6,6 +6,8 @@ from protected import router as protected_router
 from assessment import router as assessment_router
 from review import router as review_router
 from recordings import router as recordings_router
+from activities import router as activities_router
+from media import router as media_router
 
 app = FastAPI(
     title="Himma API Service",
@@ -26,6 +28,8 @@ app.add_middleware(
 app.include_router(auth_router)
 app.include_router(protected_router)
 app.include_router(assessment_router)
+app.include_router(activities_router)
+app.include_router(media_router)
 app.include_router(review_router)
 app.include_router(recordings_router)
 
